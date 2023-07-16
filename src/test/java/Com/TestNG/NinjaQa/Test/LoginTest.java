1466358ac9dfa92c532fd5c2b8333f3eed0bd2f6
Kalyankar Shashank <shashankraj2595@gmail.com>
2023-07-16 17:46:01 +0530

    Update LoginTest.java

--- src/test/java/Com/TestNG/NinjaQa/Test/LoginTest.java
+++ src/test/java/Com/TestNG/NinjaQa/Test/LoginTest.java
@@ -1,5 +1,7 @@
 package Com.TestNG.NinjaQa.Test;
 
+import Com.TestNG.NinjaQa.base.Base;
+import com.Ninja.qa.utils.Utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,29 +16,12 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 import java.util.Date;
 
-public class LoginTest {
+public class LoginTest extends Base {
 // Author
     WebDriver driver;
     @BeforeMethod
     public void setup(){
-        String browsername="firefox";
-        if(browsername.equals("chrome")){
-            driver=new ChromeDriver();
-
-            }
-            else if(browsername.equals("firefox"))
-            {
-                driver=new FirefoxDriver();
-            }
-        else if(browsername.equals("edge"))
-        {
-            driver=new EdgeDriver();
-        }
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-        driver.get("https://tutorialsninja.com/demo/");
+        driver=initailizeBrowser("chrome");
         driver.findElement(By.xpath("//span[contains(text(),'My Account')]")).click();
         driver.findElement(By.linkText("Login")).click();
 
@@ -49,7 +34,7 @@ public class LoginTest {
 
     @Test(priority=1)
     public void TC01verifyloginwithValidCredentials() {
-        driver.findElement(By.id("input-email")).sendKeys("kalyanshaan2@gmail.com");
+        driver.findElement(By.id("input-email")).sendKeys("kalyanshaan25@gmail.com");
         driver.findElement(By.id("input-password")).sendKeys("12345");
         driver.findElement(By.xpath("//body/div[@id='account-login']/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/input[1]")).click();
         Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed(), "Edit Your Account information when not displayed ");
@@ -60,7 +45,7 @@ public class LoginTest {
 
     @Test(priority=2)
     public void TC02loginwithInvalidCredentials() {
-        driver.findElement(By.id("input-email")).sendKeys("kalyanshaan25" + generateTimeStamp() + "@gmail.com");
+        driver.findElement(By.id("input-email")).sendKeys(Utils.generateEmailWithTimeStamp());
         driver.findElement(By.id("input-password")).sendKeys("12345");
         driver.findElement(By.xpath("//body/div[@id='account-login']/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/input[1]")).click();
         String actualwarningmessage = driver.findElement(By.xpath("//div[@class='alert alert-danger alert-dismissible']")).getText();
@@ -97,10 +82,6 @@ public class LoginTest {
         Assert.assertTrue(actualwarningmessage.contains(expectedWarnifmessage), "Expected Warning message is not dispalyed");
 
     }
-    public String generateTimeStamp() {
-        Date date = new Date();
-        return date.toString().replace(" ", "_").replace(":", "_");
 
-    }
 }
 
